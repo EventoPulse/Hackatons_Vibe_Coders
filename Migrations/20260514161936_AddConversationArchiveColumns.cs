c525d147596b4ab2ad154252166075041c5714b9
Javicilie0 <183185473+Javicilie0@users.noreply.github.com.>
2026-05-14 19:21:02 +0300

    Add conversation archive columns migration

---
+++ Migrations/20260514161936_AddConversationArchiveColumns.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConversationArchiveColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ArchivedByP1At",
+                table: "Conversations",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ArchivedByP2At",
+                table: "Conversations",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArchivedByP1At",
+                table: "Conversations");
+
+            migrationBuilder.DropColumn(
+                name: "ArchivedByP2At",
+                table: "Conversations");
+        }
+    }
+}
